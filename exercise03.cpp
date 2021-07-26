b67442debb7707329f79e429c6c8e37f84142cf1
it21109126 <87365271+it21109126@users.noreply.github.com>
2021-07-26 08:09:49 +0000

    Completed

--- exercise03.cpp
+++ exercise03.cpp
@@ -2,38 +2,45 @@
 
 using namespace std;
 
-int volume(int height, int width, int length);
-
 // 1. Define a structure called Box
 // have the integer data types Height, Width, Length
+struct Box
+{
+  int height, width, length ;
+};
+
+int volume(Box b);
 
 // Do not change the main function
 int main() {
-    // 2. Create a variable called box1 of the Box structure type
+     // 2. Create a variable called box1 of the Box structure type
     // int box1Height, box1Width, box1Length;
-    // 3. Create a variable called box2 of the Box structure type
+    struct Box box1 ;
     
+     // 3. Create a variable called box2 of the Box structure type
     // int box2Height, box2Width, box2Length;
+    struct Box box2 ;
+    
     int totalVolume;
     
     // 4. Input the height, width, lenght of box1 and box2
     cout << "Enter Box 1 Height : ";
-    cin >> Height of Box 1;
+    cin >> box1.height;
     cout << "Enter Box 1 Width : ";
-    cin >> Width of Box 1;
+    cin >> box1.width;
     cout << "Enter Box 1 Length : ";
-    cin >> Length of Box 1;
+    cin >> box1.length;
     
     cout << "Enter Box 2 Height : ";
-    cin >> Height of Box 2;
+    cin >> box2.height;
     cout << "Enter Box 2 Width : ";
-    cin >> Width of Box 2;
+    cin >> box2.width;
     cout << "Enter Box 2 Length : ";
-    cin >> Length of Box 2;
+    cin >> box2.length;
     
     // 5. Replace the coding below to pass the Box type structure
-    totalVolume = volume( , , )
-             + volume( , , );
+    totalVolume = volume(box1)
+             + volume(box2);
     
     cout << "Volume of Box is " << totalVolume << endl;
     
@@ -41,3 +48,7 @@ int main() {
 }
 
 // Implement the functions here
+int volume(Box b)
+{
+  return b.height * b.width * b.length ;
+}
--- exercise04.cpp
+++ exercise04.cpp
@@ -2,20 +2,33 @@
 using namespace std;
 
 void print(int len, int wth);
-void input(int len, int wth);
+void input(int &len, int &wth);
 
 // Do not change the main() function
-int main() {
+int main() 
+{
    int length = 10, width = 5;
+
    input(length, width);
+
    print(length, width);
+
    return 0;
 }
 
 // Do not change the print() function
-void print(int len, int wth) {
-   cout << "Length : " << len 
-        << ", Width  : " << wth << endl;
+void print(int len, int wth) 
+{
+   cout << "Length : " << len ;
+   cout  << ", Width  : " << wth << endl;
 }
 
 // Implement the Input Function here
+void input(int &len, int &wth)
+{
+  cout << "Enter the length : " ;
+  cin >> len ;
+
+  cout << "Enter the width : ";
+  cin >> wth ;
+}
